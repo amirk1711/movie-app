c44be38b4a0e9638747415ac1953382e705319d9
Amir Khan <ak9471714@gmail.com>
2021-05-11 18:14:09 +0530

    Add actions and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+export default function movies(state = [], action) {
+    if(action.type === 'ADD_MOVIES'){
+        return action.movies;
+    }
+    // our current state will not be changed by reducer
+    // reducer will return a new state(action.movies) and 
+    // it will be merged with the current state
+    return state;
+}
