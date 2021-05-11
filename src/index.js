d10bdf77c94ac2ffaadac4d0c450702aed6cebb9
Amir Khan <ak9471714@gmail.com>
2021-05-11 18:31:01 +0530

    Create the Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+// package imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+// file imports
 import './index.css';
 import App from './components/App';
-// import reportWebVitals from './reportWebVitals';
+import movies from './reducers';
+
+
+const store = createStore(movies);
+// console.log('store', store);
+// console.log('STATE', store.getState());
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -10,8 +19,3 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
